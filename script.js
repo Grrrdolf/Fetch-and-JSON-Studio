f411cc804d041625074c77544ad23350fc301452
Grrrdolf <herseyb71@gmail.com>
2020-12-07 19:20:15 -0600

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // TODO: add code here
+const astroContainer = document.querySelector('#container');
+
+
+fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+.then(res => res.json())
+.then(data => data.forEach((item) => {
+    let output = '';
+    output += `
+        <div class = "astronaut">
+            <div class ="bio">
+            <h2>${item.firstName} ${item.lastName}</h2>
+            <ul>
+                <li>Hours in space: ${item.hoursInSpace}</li>
+                <li>Active: ${item.active}</li>
+                <li>Skills: ${item.skills}</li>
+            </ul>
+            </div>
+            <img src = "${item.picture}" class = "avatar">
+        </div>
+    `;
+    
+    astroContainer.innerHTML += output;
+}))
+.catch((err) => console.log(err));
